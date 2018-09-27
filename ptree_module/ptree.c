420b74accb4376997f69b3be88ed5eebc3f7b87c
ubuntu <zhangl@nju.edu.cn>
2018-09-27 13:37:17 +0800

    ptree module

--- ptree_module/ptree.c
+++ ptree_module/ptree.c
@@ -25,28 +25,28 @@ static unsigned long **find_sys_call_table(void) {
 
 	return NULL;
 }
-static long sys_hello(void)
-{
-	    printk(KERN_INFO "hello, world!\n");
-	        return 0;
+
+static long sys_hello(void){
+	printk(KERN_INFO "hello, world!\n");
+	return 0;
 }
 
 static int addsyscall_init(void) {
-	    long *syscall = (long *)find_sys_call_table();
-	        oldcall = (int(*)(void))(syscall[__NR_hello]);
-		    write_cr0 (read_cr0 () & (~ 0x10000));
-		        syscall[__NR_hello] = (unsigned long) sys_hello;
-			    write_cr0 (read_cr0 () | 0x10000);
-			        printk(KERN_INFO "module load\n");
-				    return 0;
+	long *syscall = (long *)find_sys_call_table();
+	oldcall = (int(*)(void))(syscall[__NR_hello]);	    
+	write_cr0 (read_cr0 () & (~ 0x10000));	        
+	syscall[__NR_hello] = (unsigned long) sys_hello;
+	write_cr0 (read_cr0 () | 0x10000);
+	printk(KERN_INFO "module load\n");	    
+	return 0;
 }
 
 static void addsyscall_exit(void) {
-	    long *syscall = (long *) find_sys_call_table();
-	        write_cr0 (read_cr0 () & (~ 0x10000));
-		    syscall[__NR_hello] = (unsigned long) oldcall;
-		        write_cr0 (read_cr0 () | 0x10000);
-			    printk(KERN_INFO "module exit\n");
+	long *syscall = (long *) find_sys_call_table();        
+	write_cr0 (read_cr0 () & (~ 0x10000));	    
+	syscall[__NR_hello] = (unsigned long) oldcall;	        
+	write_cr0 (read_cr0 () | 0x10000);		    
+	printk(KERN_INFO "module exit\n");
 }
 
 module_init(addsyscall_init);
